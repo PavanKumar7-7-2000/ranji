f5bf9e655922c8badd077273f7513b329bc5dbaa
ranji <email>
2023-07-06 19:51:03 +0530

    query

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
   return (
     <>
       <Routes>
-        {/* <Route path="/" element={<Login setter={setter} />} /> */}
+        <Route path="/" element={<Login setter={setter} />} />
         <Route path="/" element={am ? <Imager /> : <Login setter={setter} />} />
         <Route path="/login" element={<Login setter={setter} />} />
       </Routes>
