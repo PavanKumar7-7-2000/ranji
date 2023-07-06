137a3fc9bf01f7134c4ece6419b42cefac4311dd
ranji <email>
2023-07-06 20:07:52 +0530

    query

--- src/Components/imager/Imager.jsx
+++ src/Components/imager/Imager.jsx
@@ -140,7 +140,7 @@ function Imager() {
           <Link to="/login">
             <div className="profilebox">
               {tour ? (
-                <img src={tour[2].img} height="50px" width="52px" alt="" />
+                <img src={tour[10].img} height="50px" width="52px" alt="" />
               ) : (
                 <></>
               )}
